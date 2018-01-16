e8d73a4168ca366d47faf3f32dfceb1294be9a8a
Pierre OPPEIN <poppein@SUP.local>
2018-01-16 14:54:37 +0100

    adding eslint

--- src/frontend/containers/Root.jsx
+++ src/frontend/containers/Root.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Root = () => {
-    return(
-        <div>{'Hello !'}</div>  
-    )
-}
+    return (
+        <div>{'Hello !'}</div>
+    );
+};
 
 export default Root;
