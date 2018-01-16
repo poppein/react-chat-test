f8f935caeadd5563f7e072610211a01baf697266
Pierre OPPEIN <poppein@SUP.local>
2018-01-16 20:14:49 +0100

    adding styles to slide messages in

--- src/frontend/components/ChatMessage.jsx
+++ src/frontend/components/ChatMessage.jsx
@@ -4,21 +4,34 @@ import styles from '../scss/ChatMessage.scss';
 import classNames from 'classnames';
 import reactStringReplace from 'react-string-replace';
 
-const ChatMessage = ({message}) => {
-    const parseSmiley = (messageText) => {
+class ChatMessage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {show: false};
+    }
+    componentDidMount() {
+        setTimeout(() => {
+          this.setState({show: true});
+        }, 10);
+    }
+
+    parseSmiley(messageText) {
         let parsed = reactStringReplace(messageText, '(wink)', (match, i) => (<img key={i} className='smiley' src='../assets/winking_face.png'/>));
         parsed = reactStringReplace(parsed, '(smile)', (match, i) => (<img key={i} className='smiley' src='../assets/smiling_face.png'/>));
         return parsed;
-    };
+    }
 
-    let className = classNames('chatMessage', {fromMe: message.from === 'me'}, {fromThem: message.from === 'them'}, message.styles);
-    let messageParsed = parseSmiley(message.text);
-    return (
-            <div className={className}>
-                {messageParsed}
-            </div>
-        );
-};
+    render() {
+        let {message} = this.props;
+        let className = classNames('chatMessage', {fromMe: message.from === 'me'}, {fromThem: message.from === 'them'}, message.styles, {show: this.state.show});
+        let messageParsed = this.parseSmiley(message.text);
+        return (
+                <div className={className}>
+                    {messageParsed}
+                </div>
+            );
+    }
+}
 
 ChatMessage.propTypes = {
     message: PropTypes.object.isRequired
