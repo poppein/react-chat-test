cfc47e85baf05908a9d76e531fb3056ee24afb3e
Pierre OPPEIN <poppein@SUP.local>
2018-01-16 20:18:04 +0100

    fixing scrolling issue

--- src/frontend/components/ChatDisplay.jsx
+++ src/frontend/components/ChatDisplay.jsx
@@ -4,15 +4,33 @@ import ChatMessage from './ChatMessage';
 import styles from '../scss/ChatDisplay.scss';
 import _ from 'lodash';
 
-const ChatDisplay = ({messages}) => {
-    return (
-        <div className={'chatDisplay'}>
-            {
-                _.map(messages, (message, key) => (<ChatMessage key={key} message={message}/>))
-            }
-        </div>
-    );
-};
+class ChatDisplay extends React.Component {
+    componentDidMount() {
+        this.scrollToBottom();
+      }
+
+    componentDidUpdate() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom() {
+        setTimeout(() => {
+            this.el.scrollIntoView({behavior: 'smooth'});
+        }, 200);
+    }
+
+    render() {
+        let {messages} = this.props;
+        return (
+            <div className={'chatDisplay'}>
+                {
+                    _.map(messages, (message, key) => (<ChatMessage key={key} message={message}/>))
+                }
+                <div ref={el => { this.el = el; }}/>
+            </div>
+        );
+    }
+}
 
 ChatDisplay.propTypes = {
     messages: PropTypes.array.isRequired
